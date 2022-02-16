65ca6232cf8a3035d43f1d826cf4170e2a304d8d
Nathan Cerqueira <nathan25.cerqueira@gmail.com>
2022-02-15 23:37:05 -0300

    Criado controller de candidacy.

---
+++ back-end/app/Http/Controllers/Api/CandidacyController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\NewCandidacyRequest;
+
+class CandidacyController extends Controller
+{
+    public function newCandidacy(NewCandidacyRequest $request)
+    {
+
+    }
+}

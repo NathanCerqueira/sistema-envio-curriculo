084e3df52b4aea21e0f08246beb4a5e5e337087b
Nathan Cerqueira <nathan25.cerqueira@gmail.com>
2022-02-20 17:16:18 -0300

    Rotas Principais criadas.

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,19 +1,16 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-
+import Candidatura from "@/views/Candidatura";
+import TrabalheConosco from "@/views/TrabalheConosco";
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Candidatura',
+    component: Candidatura
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/trabalhe-conosco',
+    name: 'TrabalheConosco',
+    component: TrabalheConosco
   }
 ]
 

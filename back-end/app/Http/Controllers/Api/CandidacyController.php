aef7e158d66e8552600c684991c8833f483dc04f
Nathan Cerqueira <nathan25.cerqueira@gmail.com>
2022-02-20 21:04:16 -0300

    Controlador de candidaturas já com a trait de upload e o método de criar uma nova candidatura.

--- back-end/app/Http/Controllers/Api/CandidacyController.php
+++ back-end/app/Http/Controllers/Api/CandidacyController.php
@@ -4,11 +4,35 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\NewCandidacyRequest;
+use App\Http\Traits\HandleFile;
+use App\Models\Candidacy;
 
 class CandidacyController extends Controller
 {
+    use HandleFile;
+
+    private $candidacy;
+
+    public function __construct(Candidacy $candidacy)
+    {
+        $this->candidacy = $candidacy;
+    }
+
     public function newCandidacy(NewCandidacyRequest $request)
     {
+        $path_cv = $this->upload($request->allFiles());
+
+        $payload = [
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'phone' => $request->input('phone'),
+            'path_cv' => $path_cv,
+            'portfolio' =>
+                $request->input('portfolio') !== null ? $request->input('portfolio') : null,
+            'linkedin' => $request->input('linkedin')
+        ];
+
+        $this->candidacy->save($payload);
 
     }
 }
